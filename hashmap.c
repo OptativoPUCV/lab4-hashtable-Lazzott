0c20059073e4581f3b01c000556e5ce3e938f9e4
Lazzott <163214835+Lazzott@users.noreply.github.com>
2024-04-27 22:01:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,9 +82,19 @@ void eraseMap(HashMap * map,  char * key)
       }
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
-
-
+Pair * searchMap(HashMap * map,  char * key) 
+{
+    if(map==NULL || key==NULL) return NULL;
+    long pos = hash(key,map->capacity);
+    while(map->buckets[pos]!=NULL && map->buckets[pos]->key!=NULL)
+      {
+        if(is_equal(map->buckets[pos]->key,key))
+        {
+          map->current = pos;
+          return map->buckets[pos];
+        }
+        pos = (pos+1)%map->capacity;
+      }
     return NULL;
 }
 
