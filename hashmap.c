fec390befb56dc7a1d8c9da210fac765d55c40be
Lazzott <163214835+Lazzott@users.noreply.github.com>
2024-04-27 23:00:15 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -132,7 +132,7 @@ Pair * nextMap(HashMap * map)
         if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL)
         {
           return map->buckets[map->current];
-          if map->buckest[map->current] == NULL)
+          if map->buckets[map->current] == NULL)
             {
               return NULL;
             }
