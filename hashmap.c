f7fb9143cbf559c9c53e8764ab6c33e8a72d82b4
Lazzott <163214835+Lazzott@users.noreply.github.com>
2024-04-27 21:58:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,7 +54,7 @@ void insertMap(HashMap * map, char * key, void * value)
 
 void enlarge(HashMap * map) 
 {
-    enlarge_called = 1;//no borrar (testing purposes)\
+    enlarge_called = 1; //no borrar (testing purposes)
     hashelem** aux = map->buckets;
     if(map==NULL) return;
     int new_capacity = map->capacity*2+1;
