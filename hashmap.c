18a7ceb4f66a47af37043282a74bc13bef92a154
Lazzott <163214835+Lazzott@users.noreply.github.com>
2024-04-28 00:02:35 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -126,12 +126,16 @@ Pair * firstMap(HashMap * map)
 
 Pair * nextMap(HashMap * map) 
 {
-    if(map==NULL) return NULL;
-    map->current = (map->current+1)%map->capacity;
+  if(map==NULL) return NULL;
   long posicion = map->current;
-  while(posicion < map->capacity && map->buckets[posicion] != NULL || map->buckets[posicion]->key == NULL)
+  while (posicion < map->capacity)
+    {
+      if(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL)
       {
         posicion = (posicion+1)%map->capacity;
+        map->current = posicion;
+        return map->buckets[posicion];
       }
+    }
     return NULL;
 }
